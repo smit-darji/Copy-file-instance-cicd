c1df40a4e8af1041d012fb731d63ebdf3f0b9d2f
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-11-23 11:07:07 +0530

    terraform

--- main.tf
+++ main.tf
@@ -11,15 +11,19 @@ provider "aws" {
   # Configuration options
 }
 
-resource "aws_codedeploy_app" "nodejs-express-app-2" {
+data "aws_iam_role" "nodejs_group_role" {
+  name = "smit-codedeploy-role"
+}
+
+resource "aws_codedeploy_app" "nodejs_express_app_2" {
   compute_platform = "Server"
   name             = "nodejs-express-app-2"
 }
 
-resource "aws_codedeploy_deployment_group" "example" {
+resource "aws_codedeploy_deployment_group" "nodejs_express_app_group" {
   app_name              = aws_codedeploy_app.nodejs-express-app-2.name
   deployment_group_name = "nodejs-express-app-2-group"
-  service_role_arn      = "arn:aws:iam::997817439961:role/smit-codedeploy-role"
+  service_role_arn      = data.aws_iam_role.nodejs_group_role.arn
 
   ec2_tag_set {
     ec2_tag_filter {
